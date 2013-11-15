388c72f7eb8e8cca065239b66cdebcb12818b80a
Piotr Ilczuk <piotr.ilczuk@netrat.pl>
2013-11-15 23:13:18 +0100

    moduł menu

--- js/app/modules/menu.js
+++ js/app/modules/menu.js
@@ -7,9 +7,13 @@ Menu.Models = {
         isActive: false,
         iconClass: "fa-circle",
         render: function() {
-            return '<li' + (this.isActive) ? ' class="active"' : '' + '>' + 
-                '<a href="' + this.href + '"><i class="fa ' + this.iconClass + '"> ' + 
-                    (this.name) ? window.utils.capitalize(this.href) : this.name + 
+            var activeClass = (this.attributes.isActive) ? ' class="active"' : '';
+            var name = (this.attributes.name) ? 
+            this.attributes.name : window.utils.capitalize(this.attributes.href) ;
+            return '<li' + activeClass + ' id="' + this.attributes.href + '">' + 
+                '<a href="' + this.attributes.href + '">' +
+                    '<i class="fa ' + this.attributes.iconClass + '"></i> ' + 
+                    name + 
                 '</a>' + 
             '</li>';
         }
@@ -18,37 +22,71 @@ Menu.Models = {
 
 Menu.Collection = {
     Links : Backbone.Collection.extend({
-        model: Menu.Models.Link
+        model: Menu.Models.Link,
+        render: function() {
+            var html = '';
+             _.each(this.models, function(link) {
+                html += link.render();
+            });
+            return html;
+        }
     })
 };
 
 Menu.Views = {
     Sidebar : Backbone.View.extend({
         el: $('#menu'),
+        links: [],
 
         getLinks: function() {
-            var links = [];
             switch(app.currentUser.role) {
                 case "hr":
-                    dashboard = new Link({href:"dashboard", iconClass: "fa-dashboard"});
-                    // links[] = new Link({href:"tests", iconClass: "fa-check-square-o"});
-                    // links[] = new Link({href:"dashboard", iconClass: "fa-dashboard"});
-                    // links[] = new Link({href:"dashboard", iconClass: "fa-dashboard"});
-                    // links[] = new Link({href:"dashboard", iconClass: "fa-dashboard"});
+                    this.links.push(new Menu.Models.Link({
+                        href:"dashboard", 
+                        iconClass: "fa-dashboard"
+                    }));
+                    this.links.push(new Menu.Models.Link({
+                        href:"tests",
+                        iconClass: "fa-check-square-o"
+                    }));
+                    this.links.push(new Menu.Models.Link({
+                        href:"candidates",
+                        iconClass: "fa-user"
+                    }));
+                    this.links.push(new Menu.Models.Link({
+                        href:"specialists",
+                        iconClass: "fa-group"
+                    }));
+                    break;
+                case "specialist":
+                    this.links.push(new Menu.Models.Link({
+                        href:"tests-to-check",
+                        iconClass: "fa-check-square"
+                    }));
+                    break;
             }
         },
 
         initialize: function() {
+            this.getLinks();
+            var linksCollection = new Menu.Collection.Links(this.links);
+            
             _.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
-            console.log("Menu rendered");
-            this.render(); // not all views are self-rendering. This one is.
+
+            this.render(linksCollection.render()); // not all views are self-rendering. This one is.
         },
-        render: function() {
-            $(this.el).append('<li class="active"><a id="dash" href="dashboard"><i class="fa fa-dashboard"></i> Dashboard</a></li>'+
-                '<li><a href="tests"><i class="fa fa-check-square-o"></i> Tests</a></li>'+
-                '<li><a href="candidates"><i class="fa fa-user"></i> Candidates</a></li>'+
-                '<li><a href="tests-to-check"><i class="fa fa-check-square"></i> Tests to check</a></li>'+
-                '<li><a href="specialists"><i class="fa fa-group"></i> Specialists</a></li>');
+
+        render: function(data) {
+            $(this.el).append(data);
         },
+
+        refresh: function(href) {
+            if ($('#'+href).length) {
+                if ($('.active', this.el).length) {
+                    $('.active', this.el).removeClass("active");
+                }
+                $('#'+href).addClass("active");
+            }
+        }
     })
 };
